64543220f214a918f7845c0e9e94e675b96ce348
CallieSardina <callie.sardina@bc.edu>
2022-05-03 22:24:01 -0400

    Node class for SmallAC

---
+++ Node.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, i, v, v_min, v_max, p, R):
+        self.i = i
+        self.v = v
+        self.v_min = v_min
+        self.v_max = v_max
+        self.p = p
+        self.R = R
+
+
+   
