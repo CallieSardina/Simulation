a5a0e85b7ad1f65b28be36c213c7dff4dbf5e8e8
CallieSardina <callie.sardina@bc.edu>
2022-05-06 21:22:48 -0400

    Node class for SmallBAC implementation

---
+++ NodeSmallBAC.py
@@ -0,0 +1,11 @@
+class NodeSmallBAC:
+    def __init__(self, i, v, p, R, R_low, R_high):
+        self.i = i
+        self.v = v
+        self.p = p
+        self.R = R
+        self.R_low = R_low
+        self.R_high = R_high
+
+
+   
