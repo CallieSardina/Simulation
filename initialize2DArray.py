c6442a3c3bbbacee09f9ae988ed2f7462b137956
CallieSardina <callie.sardina@bc.edu>
2022-05-12 11:38:14 -0400

    generalized for any n

--- initialize2DArray.py
+++ initialize2DArray.py
@@ -1,15 +1,16 @@
 # initialized the matrix of individual communication link probabilities for in- and out-gtroups
 def initialize2DArray(inGroup, outGroup, n):
+    frac = int(n/3)
     matrix = [[] for i in range(n)]
     for i in range(n):
         for j in range(n):
-            if(i <= 33 and j <= 33):
+            if(i <= frac and j <= frac):
                 matrix[i].append(inGroup)
             else:
-                if(33 < i <= 66 and 33 < j <= 66):
+                if(frac < i <= (frac * 2) and frac < j <= (frac * 2)):
                     matrix[i].append(inGroup)
                 else:
-                    if(66 < i <= 99 and 66 < j <= 99):
+                    if((frac * 2) < i <= (n - 1) and (frac * 2) < j <= (n - 1)):
                         matrix[i].append(inGroup)
                     else:
                         matrix[i].append(outGroup)
